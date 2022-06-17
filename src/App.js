f2e98bbfb10553014e17e464868c68c2a8005d17
appp <bashtaleh@gmail.com>
2022-06-17 16:35:57 +0430

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React from "react";
 import "./App.css";
-import Weather from "./Weather.js";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      This is coded by farzan {""}
-      <a
-        className="App-link"
-        href="https://github.com/farzanedtmi/appp"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        open-source on github
-      </a>
-      <Weather />
+      <div className="container">
+        <h1>Weather App</h1>
+        This is coded by farzan {""}
+        <a
+          className="App-link"
+          href="https://github.com/farzanedtmi/appp"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-source on github
+        </a>
+        <button type="button" class="btn btn-primary">
+          Primary
+        </button>
+      </div>
     </div>
   );
 }
